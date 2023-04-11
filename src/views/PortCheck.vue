583e207fde28173a66a61a4a1284768ec30b5ef5
xwb <programmerxwb@163.com>
2023-04-11 20:06:09 +0800

    链接和测试链接

--- src/views/PortCheck.vue
+++ src/views/PortCheck.vue
@@ -20,19 +20,19 @@
      <el-form-item label="port">
       <el-input v-model="databaseInfo.port" placeholder="3306" />
     </el-form-item>
-    <el-button  @click="conn()">
-        Create
-      </el-button>
-      <el-button  @click="testConn()">
-        Test
-      </el-button>
+    <el-button  @click="conn()">连接</el-button>
+    <el-button  @click="testConn()">测试链接</el-button>
   </el-form>
 </template>
 
 <script lang="ts" setup>
 import { reactive, ref } from 'vue'
 import { ElMessage, ElMessageBox } from 'element-plus'
-const mysql = require('mysql2');
+import { ElLoading } from 'element-plus'
+
+const fullscreenLoading = ref(false)
+const mysql = require('mysql2')
+
 const databaseInfo = reactive({
   host: '127.0.0.1',
   user: 'root',
@@ -40,6 +40,7 @@ const databaseInfo = reactive({
   database: '',
   port: '3306'
 })
+
 function conn(){
   const connection = mysql.createConnection({
     host: databaseInfo.host,
@@ -47,8 +48,7 @@ function conn(){
     password: databaseInfo.password,
     database: databaseInfo.database,
     port: databaseInfo.port
-  });
-connection
+  })
 connection.query(
   'show databases;',(err:any, result:any, fields:any) => {
     console.log('result', result)
@@ -65,7 +65,14 @@ connection.query(
 
 }
 
+/**
+ * 测试链接
+ */
 function testConn(){
+  const loading = ElLoading.service({
+    lock: true,
+    background: 'rgba(0, 0, 0, 0.7)',
+  })
   const connection = mysql.createConnection({
     host: databaseInfo.host,
     user: databaseInfo.user,
@@ -75,19 +82,21 @@ function testConn(){
     connectTimeout: 5000
   })
   connection.connect(function(err:any) {
-  if (err) {
-    ElMessageBox.alert(
-    '请确认你的连接信息是否正确','错误',
-    {confirmButtonText: 'OK',
-    type: 'error'})
-    return
-  }
+    if (err) {
+      ElMessageBox.alert('请确认你的连接信息是否正确','错误',
+      {confirmButtonText: 'OK',
+      type: 'error'})
+      loading.close()
+      return
+    } else {
+      ElMessageBox.alert('你的连接可用','成功',
+      {confirmButtonText: 'OK',
+      type: 'success'})
+    }
+    loading.close()
+})
 
-  ElMessageBox.alert(
-    '你的连接可用','成功',
-    {confirmButtonText: 'OK',
-    type: 'success'})
-});
+  
 }
 
 </script>
